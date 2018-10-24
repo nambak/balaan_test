71a8fdbce41694baa5c8b71faf7b42172a272064
남성룡 <alex.nam@azit501.com>
2018-10-24 14:30:49 +0900

    네이버 가격비교 크롤링

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,29 +3,63 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
 
 class SearchController extends Controller
 {
+    const SEARCH_URL = 'https://search.shopping.naver.com/search/all.nhn?query=';
+
     public function search(Request $request)
     {
         $sku = explode("\r\n", $request->input('sku'));
 
-        $path      = file_get_contents(storage_path() . '/sku.json', true);
-        $priceData = json_decode($path);
-        $results   = [];
+        $priceData = $this->getPriceData($sku);
 
-        foreach ($sku as $value) {
-            $result = array_filter($priceData, function ($item) use ($value) {
-                return $item->sku == $value;
-            });
-
-            usort($result, function ($first, $second) {
-                return $first->price > $second->price;
-            });
-
-            $results[$value] = $result;
-        }
+        $results = $this->sortByLowPrice($priceData);
 
         return view('search', compact('results'));
+    }
+
+    protected function sortByLowPrice($data)
+    {
+        foreach ($data as $key => $item) {
+
+            usort($item, function ($first, $second) {
+                return $first['price'] > $second['price'];
+            });
+
+            $results[$key] = $item;
+        }
+
+        return $results;
+    }
+
+    protected function getPriceData(Array $sku) {
+        $results = [];
+
+        foreach($sku as $item) {
+            $client = new Client;
+            $response = $client->get(self::SEARCH_URL .$item);
+
+            $crawler = new Crawler($response->getBody()->getContents());
+
+            $result = $crawler->filter('ul.goods_list > li')->each(function (Crawler $node) use ($item) {
+                return [
+                    'sku' => $item,
+                    'url' => $node->filter('.info a')->attr('href'),
+                    'price' => $this->convertPrice($node->filter('.info .price em .num._price_reload')->text()),
+                ];
+            });
+
+            $results[$item] = $result;
+        }
+
+        return $results;
+    }
+
+    protected function convertPrice($priceString)
+    {
+        return (int) str_replace(',', '', $priceString);
     }
 }
--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>검색결</title>
+    <title>검색결과</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
@@ -16,12 +16,16 @@
     <div class="container">
         @foreach($results as $sku => $value)
             <h1 class="is-size-3">{{ $sku }}</h1>
-            @foreach($value as $item)
-                <li>
-                    <a href="{{ $item->url }}">{{ $item->url }}</a> /
-                    <span>가격 : {{ number_format($item->price) }}</span>
-                </li>
-            @endforeach
+            @if (empty($value))
+                <span>검색결과 없음</span>
+            @else
+                @foreach($value as $item)
+                    <li style="margin-top: 10px;">
+                        <span>가격 : {{ number_format($item['price']) }}</span>
+                        <a class="button is-small is-success" href="{{ $item['url'] }}">쇼핑몰 바로가기</a>
+                    </li>
+                @endforeach
+            @endif
         @endforeach
     </div>
 </div>
